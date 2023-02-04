8dcf5ffaa19a2ce18ad5f0ef0b7102bb1d1fc5c7
charlie <charlie@multi.sh>
2023-02-04 16:23:58 +0000

    tidied up

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,10 @@
-import { Link } from "@remix-run/react";
-import React from "react";
-import { Image } from "~/components/image";
 import clsx from 'clsx';
+import { Link } from "@remix-run/react";
+import { Image } from "~/components/image";
+import { Time } from "~/components/time";
 import { CardHoverEffect, hoverClassName } from "~/components/hover-card";
-import { SiLinkedin, SiKeybase } from "react-icons/si";
-
+import { HiOutlineExternalLink } from 'react-icons/hi';
+import { SiKeybase, SiGithub, SiPrisma, SiMicrosoftazure, SiPhp, SiCsharp, SiYarn, SiGit, SiSpotify, SiMongodb, SiVisualstudiocode, SiDiscord, SiJavascript, SiGo, SiNodedotjs, SiTailwindcss, SiReact, SiRedis, SiDocker, SiTypescript, SiKubernetes, SiLinux } from "react-icons/si";
 
 export default function Index() {
   return (
@@ -32,19 +32,80 @@ export default function Index() {
         </div>
 
         <CardHoverEffect className="col-span-2 h-full">
-          <Link to="https://www.linkedin.com/in/charliemaddex/"
+          <Link to="https://keybase.io/nagieth"
             className={clsx(
-              'flex h-full items-center justify-center rounded-2xl bg-blue-500 text-4xl text-white',
+              'flex h-full items-center justify-center rounded-2xl bg-indigo-300 text-4xl text-white',
               hoverClassName,
             )}
           >
-            <span className="sr-only">LinkedIn</span>
+            <span className="sr-only">Keybase</span>
             <span className="transform-gpu transition group-hover:-rotate-[10deg] group-hover:scale-[1.3]">
-              <SiLinkedin />
+              <SiKeybase />
             </span>
           </Link>
         </CardHoverEffect>
 
+        <div className="col-span-3 flex items-center justify-center rounded-2xl bg-fuchsia-200 p-6 text-white md:col-span-2">
+          <div className="grid w-full grid-cols-4 grid-rows-4 gap-4 [&>svg]:w-full [&>svg]:text-center">
+            <SiTypescript size={24} />
+            <SiDocker size={24} />
+            <SiRedis size={24} />
+            <SiKubernetes size={24} />
+            <SiMicrosoftazure size={24} />
+            <SiLinux size={24} />
+            <SiPrisma size={24} />
+            <SiReact size={24} />
+            <SiTailwindcss size={24} />
+            <SiNodedotjs size={24} />
+            <SiGo size={24} />
+            <SiJavascript size={24} />
+            <SiYarn size={24} />
+            <SiGit size={24} />
+            <SiSpotify size={24} />
+            <SiMongodb size={24} />
+            <SiVisualstudiocode size={24} />
+            <SiDiscord size={24} />
+            <SiPhp size={24} />
+            <SiCsharp size={24} />
+          </div>
+        </div>
+
+        <Time></Time>
+
+        <CardHoverEffect className="col-span-3 h-full md:col-span-3">
+          <Link to="https://github.com/name"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={clsx(
+              'group relative flex h-full w-full flex-col justify-between overflow-hidden rounded-2xl text-white',
+              hoverClassName,
+            )}
+          >
+            <span aria-hidden className="pointer-events-none absolute inset-0 -z-20">
+              <Image
+                src="me.jpg"
+                alt="The Matrix scrolling characters effect"
+                fit="cover"
+                style={{ objectFit: 'cover' }}
+                className="brightness-[0.7]"
+              />
+              <span className="absolute inset-0 bg-neutral-900/50" />
+            </span>
+
+            <span aria-hidden className="px-6 pt-6">
+              <span className="flex justify-between">
+                <SiGithub className="text-3xl" />
+                <HiOutlineExternalLink className="text-xl opacity-50 transition duration-500 group-hover:opacity-100" />
+              </span>
+            </span>
+
+            <span className="space-y-0.5 px-6 pb-6">
+              <span className="block font-title font-bold">github</span>
+
+              <span className="block text-sm">my open source work &amp; contributions</span>
+            </span>
+          </Link>
+        </CardHoverEffect>
       </main>
     </section>
   );
---
+++ app/utils/constants.ts
@@ -0,0 +1,20 @@
+export const UKTimeFormatter = new Intl.DateTimeFormat(undefined, {
+    timeZone: 'Europe/London',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+});
+
+export const RelativeTimeFormatter = new Intl.RelativeTimeFormat('en', {
+    style: 'long',
+});
+export const dob = new Date('2001-08-02');
+export const age = new Date(Date.now() - dob.getTime()).getUTCFullYear() - 1970;
+export const hasHadBirthdayThisYear = new Date().getMonth() >= dob.getMonth() && new Date().getDate() >= dob.getDate();
+export const nextBirthdayYear = new Date().getFullYear() + (hasHadBirthdayThisYear ? 1 : 0);
+export const daysUntilBirthday = RelativeTimeFormatter.formatToParts(
+    Math.floor(
+        (new Date(nextBirthdayYear, dob.getMonth(), dob.getDay() + 1).getTime() - Date.now()) / 1000 / 60 / 60 / 24,
+    ),
+    'day',
+)[1]!.value.toString();
